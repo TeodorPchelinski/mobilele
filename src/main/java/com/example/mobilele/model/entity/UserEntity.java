1fabc7705c66de419c5cb62c29a0007bee7ed8cb
Teodor Pchelinski <teodor.pchelinski@abv.bg>
2023-10-03 02:08:51 +0300

    Adding classes for User
    UserLogin - DTO, Entity, Controller

---
+++ src/main/java/com/example/mobilele/model/entity/UserEntity.java
@@ -0,0 +1,75 @@
+package com.example.mobilele.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+
+    private String password;
+
+
+    private String firstName;
+
+
+    private String lastName;
+
+
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
